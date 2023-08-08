750a97eb6abaca383853bc97ca52154aa5fa24a4
GabrielSilveira2000 <107485163+GabrielSilveira2000@users.noreply.github.com>
2023-08-08 20:33:51 -0300

    IP, PORT dinâmico

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
 const routes = require('./config/routes')
-
+const IP = require('ip').address();
 app.use(routes)
 
 const PORT = "3000"
-const IP = "192.168.197.230"
-
-// app.set("PORT",PORT)
 
 app.listen(PORT,IP, () => {
-  console.log("Servidor Ligado em: " + IP + ":" + PORT)
+  console.log( `Servidor Ligado em: ${IP}:${PORT}`)
 });
