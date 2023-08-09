2264b8174b4f341a607aa1623d3786b177845423
GabrielSilveira2000 <107485163+GabrielSilveira2000@users.noreply.github.com>
2023-08-08 22:19:32 -0300

    Iniciando banco

---
+++ banco_dados.js
@@ -0,0 +1,24 @@
+const Banco = require("sqlite3");
+
+const db = new Banco.Database('Banco_Lanchonete.db');
+
+db.serialize(function() {
+    // Create a table
+    db.exec(`CREATE TABLE IF NOT EXISTS Cadastro (
+        id INTEGER PRIMARY KEY,
+        nome VARCHAR(60)NOT NULL, 
+        email VARCHAR(30) NOT NULL, 
+        senha VARCHAR(20) NOT NULL,
+        cpf VARCHAR(11) NOT NULL,
+        endereco VARCHAR(45) NOT NULL);
+    `);
+   
+    // Insert data into the table
+    // db.run("INSERT INTO Foo (name) VALUES ('bar')");
+   
+    // Query data from the table
+    // db.each("SELECT id, name FROM Foo", function(err, row) {
+    //   console.log(row.id + ": " + row.name);
+    // });
+   });
+db.close();
