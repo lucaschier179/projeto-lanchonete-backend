75eb3d1fbf0121331c8e64540820facf6d527f19
GabrielSilveira2000 <107485163+GabrielSilveira2000@users.noreply.github.com>
2023-08-09 20:33:27 -0300

    Update

--- banco_exemplo.sql
+++ banco_exemplo.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS Cadastro_Cliente (
         cpf VARCHAR(11) NOT NULL,
         endereco VARCHAR(45) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Cadastro_Colaborador (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(60)NOT NULL,
+        email VARCHAR(30) NOT NULL, 
+        senha VARCHAR(20) NOT NULL,
+        cpf VARCHAR(11) NOT NULL,
+        endereco VARCHAR(45) NOT NULL
+);
