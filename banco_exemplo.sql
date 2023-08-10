f30b8665720dcb163ae3e86fea4f613e99723d01
Lucas Marcelo Schier <ens-lucasschier@ugv.edu.br>
2023-08-09 22:12:45 -0300

    Update banco_exemplo.sql

--- banco_exemplo.sql
+++ banco_exemplo.sql
@@ -1,34 +1,34 @@
-CREATE TABLE IF NOT EXISTS Cadastro_Cliente (
+CREATE TABLE IF NOT EXISTS cadastro_cliente (
 	id_cliente SERIAL PRIMARY KEY,
-	nome VARCHAR(60)NOT NULL,
-	email VARCHAR(30) NOT NULL, 
-	senha VARCHAR(20) NOT NULL,
-	cpf VARCHAR(11) NOT NULL,
-	endereco VARCHAR(45) NOT NULL
+	nome_cliente VARCHAR(60)NOT NULL,
+	email_cliente VARCHAR(30) NOT NULL, 
+	senha_cliente VARCHAR(20) NOT NULL,
+	cpf_cliente VARCHAR(11) NOT NULL,
+	endereco_cliente VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Cadastro_Colaborador (
+CREATE TABLE IF NOT EXISTS cadastro_colaborador (
 	id_colaborador SERIAL PRIMARY KEY,
-	nome VARCHAR(60)NOT NULL,
-	email VARCHAR(30) NOT NULL, 
-	senha VARCHAR(20) NOT NULL,
-	cpf VARCHAR(11) NOT NULL,
-	endereco VARCHAR(45) NOT NULL,
-	idade INTEGER NOT NULL
+	nome_colaborador VARCHAR(60)NOT NULL,
+	email_colaborador VARCHAR(30) NOT NULL, 
+	senha_colaborador VARCHAR(20) NOT NULL,
+	cpf_colaborador VARCHAR(11) NOT NULL,
+	endereco_colaborador VARCHAR(45) NOT NULL,
+	idade_colaborador INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Produto (
+CREATE TABLE IF NOT EXISTS produto (
 	id_produto SERIAL PRIMARY KEY,
-	nome VARCHAR(50) NOT NULL,
-	tipo VARCHAR(20) NOT NULL,
+	nome_produto VARCHAR(50) NOT NULL,
+	tipo_produto VARCHAR(20) NOT NULL,
 	quantidade_produto INTEGER NOT NULL,
-	validade VARCHAR(10) NOT NULL
-	codigo_barra VARCHAR(20) NOT NULL,
+	validade_produto VARCHAR(10) NOT NULL,
+	codigo_barra_produto VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Estoque (
-	id SERIAL PRIMARY KEY,
-	produto_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS estoque (
+	id_estoque SERIAL PRIMARY KEY,
+	produto_id_estoque INTEGER NOT NULL,
 	quantidade_estoque INTEGER NOT NULL,
-	FOREIGN KEY (quantidade_estoque) REFERENCES Produto(quantidade_produto)
+	FOREIGN KEY (produto_id_estoque) REFERENCES produto(id_produto)
 );
