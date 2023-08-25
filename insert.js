c118758269ad0c9af813fbb84ab8066d80edde19
GabrielSilveira2000 <107485163+GabrielSilveira2000@users.noreply.github.com>
2023-08-24 22:13:01 -0300

    Update

--- insert.js
+++ insert.js
@@ -3,11 +3,15 @@ const db = require("./conexao")
 const CadastarCliente = async function inserirDados(Nome, Email, Senha, CPF, Endereco, Idade) {
   console.log(Nome, Email, Senha, CPF, Endereco, Idade)
   await db.connect()
-  const novoCliente = `INSERT INTO cadastro_cliente 
-  (nome_cliente, email_cliente, senha_cliente, cpf_cliente, endereco_cliente, idade_cliente) 
-  VALUES ($1, $2, $3, $4, $5, $6)`
-  db.query(novoCliente, [Nome, Email, Senha, CPF, Endereco, Idade])
-  return "Cadastrado Com Sucesso!"
+  try{
+    const novoCliente = `INSERT INTO cadastro_cliente 
+    (nome_cliente, email_cliente, senha_cliente, cpf_cliente, endereco_cliente, idade_cliente) 
+    VALUES ($1, $2, $3, $4, $5, $6)`
+    db.query(novoCliente, [Nome, Email, Senha, CPF, Endereco, Idade])
+    return "Cadastrado Com Sucesso!"
+  }catch{
+    return 'Qaqwq'
+  }
 }
 
 module.exports = {CadastarCliente}
