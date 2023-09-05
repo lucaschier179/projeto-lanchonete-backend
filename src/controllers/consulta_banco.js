35a71839eb99ca108ba23e44a43beb2117efc5e6
GabrielSilveira2000 <107485163+GabrielSilveira2000@users.noreply.github.com>
2023-09-05 20:44:18 -0300

    Update

--- src/controllers/consulta_banco.js
+++ src/controllers/consulta_banco.js
@@ -1,10 +1,10 @@
 const db = require("../../config/conexao_banco_de_dados")
 
-async function listar() {
+const Consulta = async function listar() {
   await db.connect()
   resultado = await db.query("SELECT * FROM cadastro_cliente")
   console.log(resultado.rows)
   await db.end()
 }
 
-listar()
+module.exports = { Consulta }
--- src/controllers/insert.js
+++ src/controllers/insert.js
@@ -3,14 +3,19 @@ const db = require("../../config/conexao_banco_de_dados")
 const CadastarCliente = async function inserirDados(Nome, Email, Senha, CPF, Endereco, Idade) {
   console.log(Nome, Email, Senha, CPF, Endereco, Idade)
   await db.connect()
-  try {
-    const novoCliente = `INSERT INTO cadastro_cliente 
-    (nome_cliente, email_cliente, senha_cliente, cpf_cliente, endereco_cliente, idade_cliente) 
-    VALUES ($1, $2, $3, $4, $5, $6)`
-    db.query(novoCliente, [Nome, Email, Senha, CPF, Endereco, Idade])
-    return "Cadastrado Com Sucesso!"
-  } catch {
-    return 'Qaqwq'
+  Cadastro_Existe = db.query("SELECT cpf_cliente FROM cadastro_cliente where cpf_cliente = " + CPF)
+  if(!Cadastro_Existe)
+    try {
+      const novoCliente = `INSERT INTO cadastro_cliente 
+      (nome_cliente, email_cliente, senha_cliente, cpf_cliente, endereco_cliente, idade_cliente) 
+      VALUES ($1, $2, $3, $4, $5, $6)`
+      db.query(novoCliente, [Nome, Email, Senha, CPF, Endereco, Idade])
+      return "Cadastrado Com Sucesso!"
+    } catch {
+      return 'Erro ao cadastrar'
+    }
+  else{
+    return 'Cadastro Já realizado', 404
   }
 }
 
