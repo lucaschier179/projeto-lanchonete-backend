3073f1b9a469cba6d84f571770123f02597710f6
Lucas Marcelo Schier <ens-lucasschier@ugv.edu.br>
2023-11-16 17:06:41 -0300

    Atualização - Front

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,10 @@ routes.get('/testeteste', function (req, res) {
   res.render('./../views/teste.ejs')
 })
 
+routes.get('/home', function (req, res) {
+  res.render('./../views/home.ejs')
+})
+
 routes.get('/', function (req, res) {
   res.render('./../views/index.ejs')
 })
@@ -42,7 +46,7 @@ routes.get("/api/obter-Cadastrados", function (req, res) {
 routes.get("/api/login", function (req, res) {
   BancoConsulta.Login(req.body.email, req.body.senha)
     .then((Resposta) => {
-      res.send(Resposta)
+      res.send('./../views/home.ejs')
     })
     .catch((error) => {
       res.send(error)
@@ -68,64 +72,5 @@ routes.delete("/api/remover-cadastro-pessoa/:nome", function (req, res) {
       res.sendStatus(error)
     })
 })
-
-
-//  CRUD - Básico - Aula 31/08/2023
-/*
-let infos = [
-  {
-    id: 1,
-    nome: "Lucas",
-    idade: 19,
-    email: "lschier179@hotmail.com",
-  },
-  {
-    id: 2,
-    nome: "Joãozinho",
-    idade: 16,
-    email: "jao123@hotmail.com",
-  },
-  {
-    id: 3,
-    nome: "Zequinha",
-    idade: 19,
-    email: "ze123kinha@hotmail.com",
-  },
-]
-
-function consulta(id) {
-  return infos.filter(info => info.id == id)
-}
-
-function consultaIndice(id) {
-  return infos.findIndex(info => info.id == id)
-}
-
-routes.get("/insomnia", function (req, res) {
-  res.json(infos)
-})
-
-routes.get("/insomnia/:id", function (req, res) {
-  res.json(consulta(req.params.id))
-})
-
-routes.post("/insomnia", function (req, res) {
-  infos.push(req.body)
-  res.send("Dados inseridos com sucesso!")
-})
-
-routes.put("/insomnia/:id", function (req, res) {
-  let index = consultaIndice(req.params.id)
-  infos[index].nome = req.body.nome
-  infos[index].idade = req.body.idade
-  res.send("Editado com sucesso!")
-})
-
-routes.delete("/insomnia/:id", function (req, res) {
-  let index = consultaIndice(req.params.id)
-  infos.splice(index, 1)
-  res.send("Deletado com sucesso!")
-})
-*/
 
 module.exports = routes
